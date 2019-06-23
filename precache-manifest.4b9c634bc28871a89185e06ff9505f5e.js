229da6bada55e79399a6ac701eab0ec835238d15
Ormadont <evgeniy1demyanov@gmail.com>
2019-06-23 18:10:00 +0400

    Updates

--- precache-manifest.4b9c634bc28871a89185e06ff9505f5e.js
+++ precache-manifest.4b9c634bc28871a89185e06ff9505f5e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dbc5f7f45780f4dc8f974dd7f8f3255b",
+    "revision": "3fd26b4f1a91a21b2bb08c2f90d70c00",
     "url": "/mt3/index.html"
   },
   {
-    "revision": "3dd5c5051837e135a3cc",
-    "url": "/mt3/static/css/main.b733c8c6.chunk.css"
+    "revision": "a1315118795d6f0f511c",
+    "url": "/mt3/static/css/main.e87ca97c.chunk.css"
   },
   {
     "revision": "ecd51a38569b599171fd",
     "url": "/mt3/static/js/2.8248a0c9.chunk.js"
   },
   {
-    "revision": "3dd5c5051837e135a3cc",
-    "url": "/mt3/static/js/main.b4dba647.chunk.js"
+    "revision": "a1315118795d6f0f511c",
+    "url": "/mt3/static/js/main.4e831b60.chunk.js"
   },
   {
     "revision": "46a5a429707ff05fbbc7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mt3/precache-manifest.d24f52ba243eeb268fe63273cf09e84b.js"
+  "/mt3/precache-manifest.4b9c634bc28871a89185e06ff9505f5e.js"
 );
 
 self.addEventListener('message', (event) => {
